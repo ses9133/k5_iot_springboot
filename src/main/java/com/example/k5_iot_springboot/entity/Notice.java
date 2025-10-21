6e49f7abc26940d2387f183629d794c7716f9bc2
ses9133 <ses9133@gmail.com>
2025-10-21 14:29:40 +0900

    20251021

--- src/main/java/com/example/k5_iot_springboot/entity/Notice.java
+++ src/main/java/com/example/k5_iot_springboot/entity/Notice.java
@@ -1,14 +1,12 @@
 package com.example.k5_iot_springboot.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "notice")
 @NoArgsConstructor
 @Setter
 @Getter
